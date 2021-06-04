189bb9d7b787973ef057a5fcb34bdcc737be8877
Jeremiah <jeremiah.d.wise@gmail.com>
2021-06-04 18:55:19 -0500

    Add recipes to main url, fix load static and extends base.html to templates

--- holiday_home/holiday_home-project/settings.py
+++ holiday_home/holiday_home-project/settings.py
@@ -130,7 +130,7 @@ LOGOUT_REDIRECT_URL = '/'
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIR = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- holiday_home/holiday_home-project/urls.py
+++ holiday_home/holiday_home-project/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('', views.HomePage.as_view(), name='home'),
     path('accounts/', include('accounts.urls', namespace='accounts')),
     path('accounts', include('django.contrib.auth.urls')),
+    path('recipes', include('recipes.urls', namespace='recipes')),
 ]
